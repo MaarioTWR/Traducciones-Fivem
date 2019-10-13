47d3b028bf152e5ca2d7f2032f80e6dd3aa3dcb1
MrPotrex <32984490+MrPotrex@users.noreply.github.com>
2019-10-13 22:11:50 +0200

    Modificado original es.lua

---
+++ esx_billing/locales/es.lua
@@ -0,0 +1,11 @@
+Locales['es'] = {
+	['invoices'] = 'Facturas',
+	['received_invoice'] = 'Has ~r~recibido~s~ una multa',
+	['paid_invoice'] = 'Has ~g~pagado~s~ una multa de ~r~$%s~s~',
+	['received_payment'] = 'Has ~g~recibido~s~ un pago de ~g~$%s~s~',
+	['player_not_online'] = 'El jugador no está conectado',
+	['no_money'] = 'No tienes dinero suficiente para pagar la multa',
+	['target_no_money'] = '¡El jugador ~r~No tiene~s~ dinero para pagar la multa!',
+}
+
+--Modificado al castellano original, antes era una traducción mala.
