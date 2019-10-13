3754a3b834c9f074dfd4620aad911adae83dbbc7
MrPotrex <32984490+MrPotrex@users.noreply.github.com>
2019-10-13 22:15:23 +0200

    Modificado original es.lua

---
+++ esx_clotheshop/locales/es.lua
@@ -0,0 +1,13 @@
+Locales['es'] = {
+  ['valid_this_purchase'] = '¿Quieres comprar esta ropa?',
+  ['yes'] = 'Si',
+  ['no'] = 'No',
+  ['not_enough_money'] = 'No tienes suficiente dinero',
+  ['press_menu'] = 'Presiona ~INPUT_CONTEXT~ para acceder a la tienda',
+  ['clothes'] = 'Ropa',
+  ['you_paid'] = 'Has pagado ~g~$%s~s~',
+  ['save_in_dressing'] = '¿Quieres darle un nombre a tu outfit?',
+  ['name_outfit'] = '¿Nombre del outfit?',
+  ['saved_outfit'] = '¡Tu outfit se ha guardado!',
+}
+--Modificado al castellano original, antes era una traducción mala.
