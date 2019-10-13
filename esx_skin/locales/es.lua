d0b5ea8ea0c8081a9d104a4fb4635e4ea1dc2ade
MrPotrex <32984490+MrPotrex@users.noreply.github.com>
2019-10-13 22:20:47 +0200

    Creado y traducido esx_skin

---
+++ esx_skin/locales/es.lua
@@ -0,0 +1,9 @@
+--MrPotrex
+Locales['es'] = {
+
+	['skin_menu'] = 'Skin Menu',
+	['use_rotate_view'] = 'Presione ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ o ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ para rotar.',
+	['skin'] = 'Cambiar skin',
+	['saveskin'] = 'Guardar skin',
+
+}
