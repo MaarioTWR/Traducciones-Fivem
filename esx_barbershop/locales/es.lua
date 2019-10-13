0ce1ed1c2fa1f0b8e4f708694b42179e1fd5bf37
MrPotrex <32984490+MrPotrex@users.noreply.github.com>
2019-10-13 22:08:27 +0200

    Modificado original es.lua

---
+++ esx_barbershop/locales/es.lua
@@ -0,0 +1,11 @@
+Locales['es'] = {
+  ['valid_purchase'] = '¿Quieres este corte de pelo?',
+  ['yes'] = 'Si',
+  ['no'] = 'No',
+  ['not_enough_money'] = 'No tienes suficente dinero',
+  ['press_access'] = 'Presione ~INPUT_CONTEXT~ para acceder al menú',
+  ['barber_blip'] = 'Barbero',
+  ['you_paid'] = 'Has pagado $%s',
+}
+
+--Modificado al castellano original, antes era una traducción mala.
